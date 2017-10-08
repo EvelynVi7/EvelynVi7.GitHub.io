9f9f6e08fd60a41a002c6815f0122bfd2855537f
EvelynVi7 <czeng@gsd.harvard.edu>
2017-10-08 11:12:19 -0400

    delete folder

--- weather app1/clear-day.js
+++
@@ -1,5 +0,0 @@
-function clearDay() {
-  noStroke();
-  fill(246,180,54);
-  ellipse(60,60,60);
-}
--- weather app1/clear-night.js
+++
@@ -1,7 +0,0 @@
-function clearNight() {
-
-  noStroke();
-  fill(238,219,115);
-  ellipse(60,60,60);
-
-}
